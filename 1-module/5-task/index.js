57fc65257e746b387ea8038d8408871e6e99aea2
Pavel Maksiutenko <pavelmaks648@gmail.com>
2020-05-25 22:48:29 +0530

    module 1, yask 5: line truncation function

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,10 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
-
+    const strLenght = str.length;
+    if (strLenght < maxlength) return str;
+    else {
+        const newStr = str.slice(0, maxlength - 1) + '…';
+        return newStr
+    }
 }
